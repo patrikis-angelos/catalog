2f74f0f199714da6d5ceffa62fbc8eddb827bdb8
patrick-angelos <angelosp07@gmail.com>
2021-05-27 20:15:34 +0300

    Remove form from Search Bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -40,16 +40,14 @@ const SearchBar = (props) => {
         className="filters background-black"
         onChange={(e) => handleFilterChange(e)}
       >
-        <form>
-          <input className="search-bar" id="q" type="text" placeholder="Search" defaultValue={filters.q} />
-          <div className="selection-wrapper flex space-between">
-            <Option id="cuisineType" options={cuisineOptions} value={filters.cuisineType} />
-            <Option id="dishType" options={dishOptions} value={filters.dishType} />
-            <Option id="mealType" options={mealOptions} value={filters.mealType} />
-          </div>
-          <a href={link} className="submit background-white sans" type="button" onClick={handleSubmit}>Search</a>
-          <button id="submit" className="submit background-white sans" type="button" onClick={handleClear}>Reset filters</button>
-        </form>
+        <input className="search-bar" id="q" type="text" placeholder="Search" defaultValue={filters.q} />
+        <div className="selection-wrapper flex space-between">
+          <Option id="cuisineType" options={cuisineOptions} value={filters.cuisineType} />
+          <Option id="dishType" options={dishOptions} value={filters.dishType} />
+          <Option id="mealType" options={mealOptions} value={filters.mealType} />
+        </div>
+        <a href={link} className="submit background-white sans" type="button" onClick={handleSubmit}>Search</a>
+        <button id="submit" className="submit background-white sans" type="button" onClick={handleClear}>Reset filters</button>
       </div>
     </div>
   );
